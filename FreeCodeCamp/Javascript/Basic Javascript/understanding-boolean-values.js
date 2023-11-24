4b80c09b9d88916c0680a79d695848d397c03a68
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 11:37:29 +0000

    Exercise : understanding-boolean-values

---
+++ FreeCodeCamp/Javascript/Basic Javascript/understanding-boolean-values.js
@@ -0,0 +1,9 @@
+//Exercise : Modify the welcomeToBooleans function so that it returns true instead of false.
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
