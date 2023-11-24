72ab3e7eb11d2399415c7b7ac92b704279cfe6aa
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 13:04:18 +0000

    Exercise : comparison-with-the-greater-than-operator

---
+++ FreeCodeCamp/Javascript/Basic Javascript/comparison-with-the-greater-than-operator.js
@@ -0,0 +1,15 @@
+// Exercise : Add the greater than operator to the indicated lines so that the return statements make sense.
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
