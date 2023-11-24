0f1099153bd46a41ede9f8167161775c810ac77e
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 12:02:23 +0000

    Exercise : comparison-with-the-equality-operator

---
+++ FreeCodeCamp/Javascript/Basic Javascript/comparison-with-the-equality-operator.js
@@ -0,0 +1,12 @@
+//Exercise : Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
+
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
