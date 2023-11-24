7ddd81712cf5186b46311fb98b72e15310eeccd6
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 11:52:18 +0000

    Exercise : use-conditional-logic-with-if-statements

---
+++ FreeCodeCamp/Javascript/Basic Javascript/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,15 @@
+//Exercise : Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
+
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if (wasThatTrue === true) {
+ return "Yes, that was true" 
+} else {
+  return "No, that was false"
+}
+
+
+  // Only change code above this line
+
+}
