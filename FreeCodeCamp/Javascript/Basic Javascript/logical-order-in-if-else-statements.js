829afd5a53c8f51da1ccd2e2af29d589a8e90296
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 17:31:24 +0000

    Exercise : logical-order-in-if-else-statements

---
+++ FreeCodeCamp/Javascript/Basic Javascript/logical-order-in-if-else-statements.js
@@ -0,0 +1,15 @@
+// Exercise : Change the order of logic in the function so that it will return the correct statements in all cases.
+
+
+function orderMyLogic(val) {
+if (val < 5) {
+  return "Less than 5";
+} else if (val < 10) {
+  return "Less than 10"
+} else {
+  return "Greater than or equal to 10";
+}
+
+}
+
+orderMyLogic(7);
