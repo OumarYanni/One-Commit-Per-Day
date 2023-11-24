0930563d8ae1a51e1a87060ac50620fb04016b41
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 13:24:08 +0000

    Exercise : comparison-with-the-less-than-or-equal-to-operator

---
+++ FreeCodeCamp/Javascript/Basic Javascript/comparison-with-the-less-than-or-equal-to-operator.js
@@ -0,0 +1,15 @@
+//Exercise : Add the less than or equal to operator to the indicated lines so that the return statements make sense.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
