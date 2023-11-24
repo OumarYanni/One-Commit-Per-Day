59a66e5f89565eca598fee4b7f70528040c99e85
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 22:01:09 +0000

    Exercise : returning-boolean-values-from-functions

---
+++ FreeCodeCamp/Javascript/Basic Javascript/returning-boolean-values-from-functions.js
@@ -0,0 +1,14 @@
+// Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b
+  /* if (a < b) {
+    return true;
+  } else {
+    return false;
+  } */
+  // Only change code above this line
+}
+
+isLess(10, 15);
