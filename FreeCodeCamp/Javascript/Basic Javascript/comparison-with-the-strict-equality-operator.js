c7d5415b5ec54f637a897f85430fd79fae4f857f
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 12:11:56 +0000

    Exercise : comparison-with-the-strict-equality-operator

---
+++ FreeCodeCamp/Javascript/Basic Javascript/comparison-with-the-strict-equality-operator.js
@@ -0,0 +1,12 @@
+// Exercise : Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
+
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
