309e769fd7bae2919f53fb59490fd22d43b9d580
Oumar Yanni <oumaryanni@gmail.com>
2023-11-24 13:18:46 +0000

    Exercise : comparison-with-the-less-than-operator

---
+++ FreeCodeCamp/Javascript/Basic Javascript/comparison-with-the-less-than-operator.js
@@ -0,0 +1,16 @@
+// Exercise : Add the less than operator to the indicated lines so that the return statements make sense.
+
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
