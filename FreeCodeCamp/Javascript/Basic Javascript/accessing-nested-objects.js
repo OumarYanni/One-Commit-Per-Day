2b756f1b96aea9c5d04afe8e19663569ba4f2648
Oumar Yanni <oumaryanni@gmail.com>
2023-11-29 17:29:41 +0000

    Exercise : accessing-nested-objects

---
+++ FreeCodeCamp/Javascript/Basic Javascript/accessing-nested-objects.js
@@ -0,0 +1,18 @@
+/* Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation. */
+
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+console.log(gloveBoxContents)
